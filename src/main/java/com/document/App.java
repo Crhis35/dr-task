3854066366784c198e137a9cce9e48137894f164
Crhistian Caraballo <cristianjcj35@gmail.com>
2023-02-14 20:39:31 -0500

    Added writer

--- src/main/java/com/document/App.java
+++ src/main/java/com/document/App.java
@@ -1,9 +1,14 @@
 package com.document;
 
-import java.util.List;
-
+import tech.tablesaw.api.BooleanColumn;
 import tech.tablesaw.api.DoubleColumn;
 import tech.tablesaw.api.Table;
+import tech.tablesaw.plotly.Plot;
+import tech.tablesaw.plotly.api.HorizontalBarPlot;
+import tech.tablesaw.plotly.components.Figure;
+import tech.tablesaw.plotly.components.Layout;
+import tech.tablesaw.plotly.traces.PieTrace;
+import static tech.tablesaw.aggregate.AggregateFunctions.mean;
 
 /**
  * Hello world!
@@ -12,7 +17,53 @@ import tech.tablesaw.api.Table;
 public class App {
     public static void main(String[] args) {
         Table t = ExcelReader.readFileFromTerminal();
-        System.out.println(t.print());
+        processData(t);
+        t = t.sortDescendingOn("globalScore");
+        print(t);
+        plots(t);
+        try {
+            ExcelReader.writeTable(t);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
 
+    public static void print(Table t) {
+        System.out.println(
+                Colors.BLUE_BACKGROUND_BRIGHT + "Standard Deviation: "
+                        + t.doubleColumn("globalScore").standardDeviation() + Colors.RESET);
+    }
+
+    public static void processData(Table t) {
+        DoubleColumn scores = DoubleColumn.create("globalScore");
+        BooleanColumn approvers = BooleanColumn.create("approved");
+
+        t.forEach(row -> {
+            double globalScore = row.getNumber("score1") * 0.2 +
+                    row.getNumber("score2") * 0.3 +
+                    row.getNumber("score3") * 0.5;
+            scores.append(globalScore);
+            approvers.append(globalScore >= 3);
+        });
+        t.addColumns(scores, approvers);
+
+    }
+
+    public static void plots(Table t) {
+        Table t2 = t.countBy(t.categoricalColumn("approved"));
+        PieTrace trace = PieTrace.builder(t2.categoricalColumn("approved"),
+                t2.numberColumn("Count")).build();
+        Layout layout = Layout.builder().title("Total students by approved").build();
+
+        Plot.show(new Figure(layout, trace));
+        Table means = t.summarize("globalScore", mean).by("approved");
+
+        // Plot
+        Plot.show(
+                HorizontalBarPlot.create(
+                        "Means by approved", // plot title
+                        means, // table
+                        "approved", // grouping column name
+                        "mean [globalScore]")); // numeric column name
     }
 }
--- src/main/java/com/document/ExcelReader.java
+++ src/main/java/com/document/ExcelReader.java
@@ -1,41 +1,74 @@
 package com.document;
 
-import java.util.ArrayList;
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
 
 import tech.tablesaw.api.Table;
 import tech.tablesaw.io.xlsx.*;
+import tech.tablesaw.io.csv.*;
 
 public class ExcelReader {
-    private static Table readFile(String path) {
+    static String file = "";
+
+    private static Table readFile(String path) throws IOException {
         XlsxReader reader = new XlsxReader();
         XlsxReadOptions options = XlsxReadOptions.builder(path).build();
+
         return reader.read(options);
+    }
+
+    public static void writeTable(Table t) throws IOException {
+        String path = ExcelReader.file.split("[.]")[0] + System.nanoTime() + ".csv";
+        CsvWriter writer = new CsvWriter();
+        CsvWriteOptions options = CsvWriteOptions.builder(path).build();
+
+        writer.write(t, options);
+        System.out.print(Colors.PURPLE_BACKGROUND_BRIGHT + "File generated on path: " + path + Colors.RESET);
+
     }
 
     public static Table readFileFromTerminal() {
         Scanner s = new Scanner(System.in);
         Table result = Table.create();
-        ArrayList<String> validColumns = new ArrayList<>(Arrays.asList("id", "name", "score1", "score2", "score3"));
-        System.out.println("Please enter a path from excel (xlsx)");
+        System.out.print(Colors.GREEN + "Please enter a path from excel (xlsx): " + Colors.RESET);
+        Map<String, String> validColumns = Map.of(
+                "id", "STRING,INTEGER",
+                "name", "STRING",
+                "score1", "INTEGER,DOUBLE",
+                "score2", "INTEGER,DOUBLE",
+                "score3", "INTEGER,DOUBLE");
         try {
             while (true) {
                 String input = s.nextLine();
                 try {
                     result = readFile(input);
-                    List<String> columnNames = result.columnNames(); // returns all column names
-                    for (String value : validColumns) {
-                        if (!columnNames.contains(value)) {
-                            throw new Exception("Invalid column " + value);
+                    List<String> columnNames = result.columnNames();
+
+                    for (Map.Entry<String, String> entry : validColumns.entrySet()) {
+                        String key = entry.getKey();
+                        List<String> types = Arrays.asList(entry.getValue().split(","));
+                        String currentType = result.column(key).type().toString();
+                        if (!columnNames.contains(key)) {
+                            throw new Exception("Invalid column " + key);
+                        }
+                        if (!types.contains(currentType)) {
+                            throw new Exception(
+                                    String.format("Error type on column: %s \nExpected types: %s \nReceived types: %s",
+                                            key + Colors.RESET,
+                                            Colors.GREEN_BACKGROUND + types + Colors.RESET, Colors.RED_BACKGROUND_BRIGHT
+                                                    + currentType + Colors.RESET));
                         }
                     }
-
+                    ExcelReader.file = input;
                     break;
                 } catch (Exception nfe) {
+                    System.err.print(Colors.RED);
                     System.err.println(nfe);
-                    System.err.println("Error: " + nfe.getMessage());
+                    System.err.println("Error: " + nfe.getMessage() + Colors.RESET);
+                    System.out.print(Colors.BLUE + "Please enter a valid path from excel (xlsx): " + Colors.RESET);
                 }
 
             }
